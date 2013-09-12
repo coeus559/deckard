e6863b24b089f29b8334359b1b74e26b1148d718
coeus559 <coeus559@gmail.com>
2013-09-12 16:27:35 -0700

    Update countdown.py
    add useless y2.038k bug calculator

--- modules/countdown.py
+++ modules/countdown.py
@@ -27,3 +27,8 @@ def simcity(phenny, input):
     diff = datetime.datetime(2013, 03, 5) - datetime.datetime.now()
     phenny.say("SimCity 5 is " + str(diff.days) + " days, " + str(diff.seconds/60/60) + " hours, and " + str(diff.seconds/60 - diff.seconds/60/60 * 60) + " minutes from now pharix! lol.")
 simcity.rule = '(simcity?.)$'
+
+def y2k(phenny, input):
+    diff = datetime.datetime(2038, 01, 19, 03, 14, 07) - datetime.datetime.now()
+    phenny.say("The end of the Unixverse is " + str(diff.days) + " days, " + str(diff.seconds/60/60) + " hours, and " + str(diff.seconds/60 - diff.seconds/60/60 * 60) + " minutes from now.")
+y2k.rule = '(y2k?.)$'
